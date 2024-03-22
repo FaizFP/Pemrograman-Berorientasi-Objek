64d53048cd65bba7a19976b8ab1ddb0a14a746f4
TASANA <154037776+FaizFP@users.noreply.github.com>
2024-03-22 17:03:07 +0700

    Add files via upload

---
+++ Praktikum3_2ReturnAtrribute.py
@@ -0,0 +1,34 @@
+#Faiz Firdaus Priyanto 
+#06400230005
+#Informatika
+
+
+
+class Persegi:
+
+    def __init__(self):
+          #Identitas
+        self.nama  = "Faiz Firdaus Priyanto"
+        self.nim   = "064002300005"
+        self.studi = "Teknik Informatika"
+        self.panjang = 20
+        self.lebar = 12
+
+    def segitiga(self):
+        return self.panjang * self.lebar  #rumus persegi 
+    
+    def tampilan(self):  #tampilan user 
+        print(self.nama , self.nim , self.studi)  
+        print("---->PROGRAM MENGHITUNG PERSEGI PANJANG<----")
+        print("Persegi panjang dengan panjang", self.panjang, "cm, lebar", self.lebar, "memiliki luas sekitar", self.segitiga(), "cm^2")
+
+def main():  #Pemanggilan fungsi 
+    obj_persegi = Persegi()
+    obj_persegi.tampilan()
+
+if __name__ == "__main__":
+    main()
+
+
+    
+
