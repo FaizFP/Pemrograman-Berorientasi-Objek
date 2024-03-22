71033ff0e509fd25efbf26b6d8c8f7e47f0725e6
TASANA <154037776+FaizFP@users.noreply.github.com>
2024-03-22 17:27:45 +0700

    Update Praktikum3_2ReturnAtrribute.py

--- Praktikum3_2ReturnAtrribute.py
+++ Praktikum3_2ReturnAtrribute.py
@@ -1,8 +1,8 @@
 #Faiz Firdaus Priyanto 
 #06400230005
 #Informatika
-
-
+#program yang dapat menampilkan hasil perhitungan luas sebuah persegi panjang, 
+#dimana panjang dan lebarnya di set sebagai parameter saat pembuatan object (panjang dan lebar persegi panjang bebas).
 
 class Persegi:
 
@@ -14,17 +14,17 @@ class Persegi:
         self.panjang = int(input("MAsukkan angka : "))
         self.lebar = int(input("MAsukkan angka : "))
 
-    def segitiga(self):
+    def luas_persegi(self):
         return self.panjang * self.lebar  #rumus persegi 
     
-    def tampilan(self):  #tampilan user 
+    def tampilan_data(self):  #tampilan user 
         print(self.nama , self.nim , self.studi)  
         print("---->PROGRAM MENGHITUNG PERSEGI PANJANG<----")
         print("Persegi panjang dengan panjang", self.panjang, "cm lebar", self.lebar, "memiliki luas sekitar", self.segitiga(), "cm^2")
 
 def main():  #Pemanggilan fungsi 
     oPersegi = Persegi()
-    oPersegi.tampilan()
+    oPersegi.tampilan_data()
 
 if __name__ == "__main__":
     main()
